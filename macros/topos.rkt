5a8ab5ae12602feb4fd3d9246153a86fc977d38f
Jon Sterling <jon@jonmsterling.com>
2022-01-07 19:46:32 +0100

    deploy

---
+++ macros/topos.rkt
@@ -0,0 +1,68 @@
+#lang at-exp racket
+
+(require "macro-kit.rkt")
+
+(define-global (Sub x)
+  "_{" x "}")
+(define-global (Sup x)
+  "^{" x "}")
+
+(define-local ShSymbol
+  @tex{\boldsymbol{\mathcal{S}}})
+
+(define-local GlSymbol
+  @tex{\boldsymbol{\mathcal{G}}})
+
+(define-global (Con x)
+  @tex{\mathsf{@x}})
+
+(define-global (Sh X)
+  ShSymbol
+  (Sub X))
+
+(define-global (GL X)
+  GlSymbol
+  (Sub X))
+
+(define-global (Gl x)
+  (Con "gl")
+  (Sub x))
+
+(define-local (overline x)
+  @tex{\overline{@x}})
+
+(define-global (OpGL x)
+  (overline GlSymbol)
+  (Sub x))
+
+(define-global (OpGl x)
+  (overline (Con "gl"))
+  (Sub x))
+
+(define-global (Cod C)
+  (Con "cod")
+  (Sub C))
+
+(define-local (bold kwd)
+  @tex{\mathbf{@kwd}})
+
+(define-global (TOP E)
+  (bold "Top")
+  (Sub E))
+
+(define-global (Idn x)
+  "1" (Sub x))
+
+(define-global (TotCat E)
+  @tex{\widetilde{@E}})
+
+(define-global (SelfIx B)
+  @tex{\underline{@B}})
+
+(define-global (Sl E e)
+  E (Sub @tex{/@e}))
+
+(define-global (InvImg f)
+  f (Sup @tex{*}))
+
+(publish-macro-library 'topos)
