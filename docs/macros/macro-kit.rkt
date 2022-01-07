1c694ce61fc149a2605ce794ae48e2d65d917e36
Jon Sterling <jon@jonmsterling.com>
2022-01-07 14:15:16 +0100

    render

--- docs/macros/macro-kit.rkt
+++ docs/macros/macro-kit.rkt
@@ -1,7 +1,8 @@
 #lang at-exp racket
+
 (require json)
 
-(provide emit render-macros-to-tex render-macros-to-mathjax)
+(provide tex define-global define-local publish-macro-library)
 
 (struct macro-repr (name arity inst) #:transparent)
 
@@ -15,31 +16,45 @@
 (define (write-macro-repr-latex repr port)
   (display "\\newcommand\\" port)
   (display (macro-repr-name repr) port)
-  (display "[" port)
-  (display (macro-repr-arity repr) port)
-  (display "]{" port)
+  (when (> (macro-repr-arity repr) 0)
+    (display "[" port)
+    (display (macro-repr-arity repr) port)
+    (display "]" port))
+  (display "{" port)
   (display (macro-repr-inst repr) port)
   (displayln "}" port))
 
 (define macro-set (make-parameter (mutable-set)))
 
+(define (macro-list)
+  (define (lt? repr1 repr2)
+    (string<? (macro-repr-name repr1) (macro-repr-name repr2)))
+  
+  (sort (set->list (macro-set)) lt?))
+
 (define (render-macros-to-tex filename)
   (call-with-output-file filename #:exists 'replace
     (λ (port)
-      (for ([repr (in-set (macro-set))])
+      (for ([repr (in-list (macro-list))])
         (write-macro-repr-latex repr port)))))
 
 (define (render-macros-to-mathjax filename)
-  (define macros (set->list (macro-set)))
+  (define macros (macro-list))
   (define n (length macros))
   (call-with-output-file filename #:exists 'replace
     (λ (port)
       (write-json (macro-reprs->jsexpr macros) port))))
 
+(define (publish-macro-library sym)
+  (define (filename ext)
+    (string-append "../assets/macros/" (symbol->string sym) "." ext))
+  (render-macros-to-tex (filename "sty"))
+  (render-macros-to-mathjax (filename "json")))
+
 (define (index a b)
   (let [(tail (member a (reverse b)))]
     (and tail (length (cdr tail)))))
-
+ 
 (define-syntax id->string
   (lambda (stx)
     (syntax-case stx ()
@@ -52,15 +67,29 @@
   (string-append
    "#"
    (number->string (+ 1 (index arg args)))))
- 
-(define-syntax-rule (emit (id arg ...) bdy ...)
+
+(define (tex . args)
+  (string-append* args))
+
+(define-syntax-rule (define-local head bdy ...)
   (begin
-    (define (id arg ...) (string-append bdy ...))
-    (let*
-        ([name (id->string id)]
-         [args (list (syntax->datum #'arg) ...)]
-         [arity (length args)]       
-         [inst
-          (let ([arg (name-arg (syntax->datum #'arg) args)] ...)
-            (string-append bdy ...))])
-      (set-add! (macro-set) (macro-repr name arity inst)))))
+    (define head (tex bdy ...))))
+ 
+(define-syntax define-global
+  (syntax-rules ()
+    [(emit (id arg ...) bdy ...)
+     (begin
+       (define (id arg ...) (string-append "" bdy ...))
+       (let*
+           ([name (id->string id)]
+            [args (list (syntax->datum #'arg) ...)]
+            [arity (length args)]       
+            [inst
+             (let ([arg (name-arg (syntax->datum #'arg) args)] ...)
+               (tex bdy ...))])
+         (set-add! (macro-set) (macro-repr name arity inst))))]
+    [(emit id bdy ...)
+     (begin
+       (define id (tex bdy ...))
+       (let ([name (id->string id)])
+         (set-add! (macro-set) (macro-repr name 0 id))))]))
