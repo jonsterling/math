0c0486446ac1415f6c0021c87da449204a703d2c
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-15 18:48:15 +0100

    chore: add todo note

--- _plugins/import_tag.rb
+++ _plugins/import_tag.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# TODO: this still exists only to avoid a crash with jekyll-feed, and we should find a better solution
+# TODO: this still exists only to avoid a crash with jekyll-feed, and we should find a better solution. This can mean ensuring jekyll-feed skips nodes, or choosind a different syntax for imports, or...
 class ImportTag < Liquid::Tag
   def initialize(tag_name, slug, tokens)
   end
