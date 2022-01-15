0d231237169c032339123e635b04f139b12a7aaa
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-15 18:35:20 +0100

    feat: rewrite of gerby-like behaviour in Gnarly

--- _plugins/import_tag.rb
+++ _plugins/import_tag.rb
@@ -1,30 +1,11 @@
-# This code is roughly based on https://github.com/koraktor/jekyll/tree/import-tag
-#
 # frozen_string_literal: true
 
+# TODO: this still exists only to avoid a crash with jekyll-feed, and we should find a better solution
 class ImportTag < Liquid::Tag
   def initialize(tag_name, slug, tokens)
-    super
-    @slug = slug.strip
   end
 
   def render(context)
-    registers = context.registers
-    site = registers[:site]
-    page = registers[:page]
-
-    imported = Jekyll::ImportedNode.new(site, site.source, "#{@slug}.md", superpage: page)
-    Jekyll::NodeGraph.new(site.data).register_subpage(page, imported)
-
-    # tracks dependencies like Jekyll::Tags::IncludeTag so --incremental works
-    if page&.key?('path')
-      path = site.in_source_dir(page['path'])
-      dependency = site.in_source_dir(imported.path)
-      site.regenerator.add_dependency(path, dependency)
-    end
-
-    imported.render(site.layouts, site.site_payload)
-    imported.output
   end
 end
 
