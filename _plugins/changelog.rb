dc4773e904a8f687749c1c873ce6f9fe0e3dd34c
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-19 00:23:55 +0100

    fix: broken style and git command for changelog

--- _plugins/changelog.rb
+++ _plugins/changelog.rb
@@ -5,12 +5,11 @@ require 'date'
 def get_git_data
   stdout, stderr, status = Open3.capture3(
     [
-      "git rev-list",
-      "HEAD",
-      "--no-commit-header",
-      "--format=format:' %H %at %s'",
-      "--objects",
-      "--in-commit-order",
+      "git log",
+      "--pretty='format:%H %at %s'",
+      "--name-status",
+      "--no-renames",
+      "--diff-filter=ADM",
       "--",
       "_lectures/*.md",
       "_nodes/*.md",
@@ -21,24 +20,20 @@ end
 
 def parse_git_data(data)
   [].tap do |changelog|
-    data.lines.map do |line|
-      case line
-      # commit objects are detected by leading space
-      when /^ /
-        hash, timestamp, subject = line.strip.split(' ', limit=3)
-        changelog << {
-          "hash" => hash,
-          "timestamp" => Time.at(timestamp.to_i),
-          "subject" => subject,
-          "nodes" => [],
-        }
-      # interesting blob objects are detected by path
-      when /^[0-9a-f]{40} (?<file>_(lectures|nodes)\/.+\.md)$/
-        changelog.last.fetch("nodes") << Regexp.last_match[:file]
+    data.split("\n\n").each do |block|
+      commit, *diff = block.split("\n")
+      hash, timestamp, subject = commit.split(' ', limit=3)
+      changelog << {
+        "hash" => hash,
+        "timestamp" => Time.at(timestamp.to_i),
+        "subject" => subject,
+        "diffs" => [],
+      }
+      diff.each do |line|
+        status, path = line.split("\t")
+        changelog.last.fetch("diffs") << { "status" => status, "path" => path }
       end
     end
-    changelog.each_cons(2) { |(a,b)| a["nodes"] = b["nodes"] }
-    changelog.pop
   end
 end
 
@@ -49,8 +44,10 @@ module Changelog
       changelog = site.pages.find { |page| page.relative_path == "changelog.md" }
       commits = parse_git_data(get_git_data)
       commits.each do |commit|
-        commit["nodes"].map! do |path|
-          nodes.find { |node| node.relative_path == path }
+        commit["diffs"].each do |diff|
+          diff["node"] = nodes.find do |node|
+            node.relative_path == diff["path"]
+          end
         end
       end
       changelog.data['commits'] = commits
