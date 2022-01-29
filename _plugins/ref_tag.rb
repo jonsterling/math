e385478416be6b35d158da17060337a5ea14ba38
Jon Sterling <jon@jonmsterling.com>
2022-01-29 12:17:37 +0100

    add support for cref/pref

--- _plugins/ref_tag.rb
+++ _plugins/ref_tag.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
-
-# A tag to generate a [XXXXX] reference link to a node
+#
 class RefTag < Liquid::Tag
   def initialize(tag_name, slug, tokens)
     super
@@ -11,10 +10,42 @@ class RefTag < Liquid::Tag
     registers = context.registers
     site = registers[:site]
     node = site.collections['nodes'].docs.detect { |doc| doc.data['slug'] == @slug }
-    "<a href='#{site.baseurl}#{node.url}' role='tooltip' aria-label='#{node.data["title"]}' class='slug'>[#{@slug}]</a>"
+    info = NodeInfo.new node.data
+    "<a href='#{site.baseurl}#{node.url}' role='tooltip' aria-label='#{info.aria_label}' class='slug'>[#{@slug}]</a>"
   end
 end
 
+class CleverRefTag < Liquid::Tag
+  def initialize(tag_name, slug, tokens)
+    super
+    @slug = slug.strip
+  end
+
+  def render(context)
+    registers = context.registers
+    site = registers[:site]
+    node = site.collections['nodes'].docs.detect { |doc| doc.data['slug'] == @slug }
+    info = NodeInfo.new node.data
+    "<a href='#{site.baseurl}#{node.url}' role='tooltip' aria-label='#{info.aria_label}' class='cref'>#{info.display_index} <span class='slug'>[#{@slug}]</span></a>"
+  end
+end
+
+class ParenCleverRefTag < Liquid::Tag
+  def initialize(tag_name, slug, tokens)
+    super
+    @slug = slug.strip
+  end
+
+  def render(context)
+    registers = context.registers
+    site = registers[:site]
+    node = site.collections['nodes'].docs.detect { |doc| doc.data['slug'] == @slug }
+    info = NodeInfo.new node.data
+    "(<a href='#{site.baseurl}#{node.url}' role='tooltip' aria-label='#{info.aria_label}' class='cref'>#{info.display_index} <span class='slug'>[#{@slug}]</span></a>)"
+  end
+end
+
+
 Liquid::Template.register_tag('ref', RefTag)
-Liquid::Template.register_tag('cref', RefTag)
-Liquid::Template.register_tag('pref', RefTag)
+Liquid::Template.register_tag('cref', CleverRefTag)
+Liquid::Template.register_tag('pref', ParenCleverRefTag)
