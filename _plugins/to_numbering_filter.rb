6f36e744a76c639e820ff3cd4cb3e493f88a2bfc
Jon Sterling <jon@jonmsterling.com>
2022-01-29 17:01:09 +0100

    Fix broken fix :)

--- _plugins/to_numbering_filter.rb
+++ _plugins/to_numbering_filter.rb
@@ -1,19 +1,17 @@
-def clicks_to_numbering(clicks)
-  label = ""
-  # NOTE: we skip the first one because it's the lesson
-  clicks[1..].each do |tick|
-    label << case tick["clicker"]
-    when "section" then ".#{tick["value"] + 1}"
-    when "result" then "·#{(tick["value"] + 97).chr}"
-    else ".#{tick["value"]}"
-    end
-  end
-  label[1..]
-end
-
 module ToNumberingFilter
+  extend self
+
   def to_numbering(clicks)
-    clicks_to_numbering clicks
+    label = ""
+    # NOTE: we skip the first one because it's the lesson
+    clicks[1..].each do |tick|
+      label << case tick["clicker"]
+      when "section" then ".#{tick["value"] + 1}"
+      when "result" then "·#{(tick["value"] + 97).chr}"
+      else ".#{tick["value"]}"
+      end
+    end
+    label[1..]
   end
 end
 
